28910dd7ece828efa8f8ed5eaef5b1d87c98b4a4
Caique Nascimento <102535816+CaiqueSN@users.noreply.github.com>
2022-06-03 15:34:52 -0300

    js

---
+++ js/varLetConst.js
@@ -0,0 +1,39 @@
+//var weight
+
+//console.log(typeof weight)
+
+//let name = 'Caique'
+//let age = 26
+//let stars = 30.1
+//let isSubscribed = true
+
+let student = {
+    name: 'Caique',
+    age: 26,
+    weight: 60.1,
+    isSubscribed: true
+}
+
+//console.log(`${student.name} de idade ${student.age} anos pesa ${student.weight} KG`)
+
+let students = []
+
+students = [
+    student
+]
+
+console.log(students[0])
+
+let caique = {
+    name: 'João',
+    age: 26,
+    weight: 60.1,
+    isSubscribed: true
+}
+students = [
+    student, caique
+]
+console.log(students[1])
+
+console.log(a)
+var a = 1
